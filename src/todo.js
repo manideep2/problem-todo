46b03645b2037eeda9e9a2b4223d236effb0291b
manideep2 <manideep.github@gmail.com>
2021-06-04 14:35:41 +0530

    1 file modified

--- src/todo.js
+++ src/todo.js
@@ -52,9 +52,12 @@ class TodoApp extends React.Component {
     render() {
       return (
         <ul>
+        
           {this.props.items.map(item => (
             <li key={item.id}>{item.text}</li>
           ))}
+          
+          
         </ul>
       );
     }
